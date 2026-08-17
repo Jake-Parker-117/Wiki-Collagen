89551747cf619a90c0d1bcafc19b6630e00658f6
Jake-Parker-117 <jtp.313.183@gmail.com>
2026-08-17 21:20:15 +0100

    Debugging 	modified:   main.py 	modified:   templates/loading.html

--- main.py
+++ main.py
@@ -417,5 +417,5 @@ if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=5000)
 
     # Run Flask production server with Waitress
-    serve(app, host='0.0.0.0', port=5000, threads=1, max_request_body_size=1073741824) 
+    serve(app, host='0.0.0.0', port=5000, threads=2, max_request_body_size=1073741824) 
     #switching to single thread on waitress so background gpu threads take care of the processing
